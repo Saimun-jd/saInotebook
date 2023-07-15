50b80850eaa552659b44889ce77a6c0e09700710
Saimunjd <saimuncoder@gmail.com>
2023-07-16 00:40:25 +0600

    Integrated password hashing and jwt auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router()
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "fata$$nigga";
 
 // Post request handling for creating a user
 router.post('/createuser', [
@@ -20,14 +24,21 @@ router.post('/createuser', [
             return res.status(400)
                 .json({error: "email already exists!!!"});
         }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email
         })
-        res.json(
-            {result: `Success!! created a user with: Name: ${user.name}, Email: ${user.email}`
-        });
+        const data = {
+            user: {
+                id: user.id 
+            }
+        };
+        const authtoken = jwt.sign(data, JWT_SECRET);
+
+        res.json({authtoken});
     } catch(error){
         console.error(error.message);
         res.status(500)
